3233972736abe7692690cfc061e3b1ebef852b5d
Rafi Madani <rafi.madani@ui.ac.id>
2023-05-07 03:10:31 +0700

    register all type of account, login, portal udah

--- reg_log/views.py
+++ reg_log/views.py
@@ -6,5 +6,14 @@ def login(request):
 def register(request):
     return render(request, 'register.html')
 
+def register_athlete(request):
+    return render(request, 'athlete.html')
+
+def register_coach(request):
+    return render(request, 'coach.html')
+
+def register_umpire(request):
+    return render(request, 'umpire.html')
+
 def portal(request):
     return render(request, 'portal.html')
