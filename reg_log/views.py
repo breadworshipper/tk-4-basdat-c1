ee7577196b9be428d815987ee77725b5007410ea
AndiRaihan <dzaky.raihan@gmail.com>
2023-05-16 16:28:59 +0700

    refactored code

--- reg_log/views.py
+++ reg_log/views.py
@@ -13,6 +13,7 @@ def register_umpire(request):
     return render(request, 'umpire.html')
 
 def register_athlete(request):
+    # Benerin connect kalo buat nanti PROD
     if (request.method == 'POST'):
         db_connection = psycopg2.connect(
             host="localhost",
@@ -34,9 +35,8 @@ def register_athlete(request):
         
         try:
             create_member(cursor, id_user, nama, email)
-            query_insert_atlet = """INSERT INTO atlet (id, tgl_lahir, negara_asal, play_right, height, world_rank, jenis_kelamin) 
-            VALUES (%s, %s, %s, %s, %s, NULL, %s)"""
-            cursor.execute(query_insert_atlet, (id_user, tanggal_lahir, negara, play_type, tinggi_badan , jenis_kelamin))
+            create_atlet(cursor, id_user, negara, tanggal_lahir, play_type,
+                         tinggi_badan, jenis_kelamin)
             db_connection.commit()
             # TODO: redirect ke suatu page
             db_connection.close()
@@ -46,8 +46,18 @@ def register_athlete(request):
             redirect('reg_log:register-atlet')
     return render(request, 'athlete.html')
 
+def create_atlet(cursor, id_user, negara, tanggal_lahir, play_type, 
+                 tinggi_badan, jenis_kelamin):
+    query_insert_atlet = """INSERT INTO atlet (id, tgl_lahir, negara_asal, 
+        play_right, height, world_rank, jenis_kelamin) VALUES (%s, %s, %s, %s, 
+        %s, NULL, %s)"""
+    cursor.execute(query_insert_atlet, (id_user, tanggal_lahir, 
+                                        negara, play_type, tinggi_badan, 
+                                        jenis_kelamin))
+
 def register_coach(request):
     if (request.method == 'POST'):
+        # TODO: Benerin connect kalo nanti buat PROD
         db_connection = psycopg2.connect(
             host="localhost",
             database="postgres",
@@ -64,26 +74,20 @@ def register_coach(request):
             kategori = request.POST.getlist('kategori')
             tanggal_mulai = request.POST.get('tanggal-mulai')
             
-            print(tanggal_mulai)
-            
             create_member(cursor, id_user, nama, email)
-            query_insert_pelatih = """INSERT INTO pelatih (id, tanggal_mulai)
-            VALUES (%s, %s)"""
-            cursor.execute(query_insert_pelatih, (id_user, tanggal_mulai))
             
-            spesialisasi_map = get_spesialisasi(cursor)
-            query_inserti_pelatih_spesialisasi = """INSERT INTO pelatih_spesialisasi (id_pelatih, id_spesialisasi)
-            VALUES (%s, %s)"""
-            pelatih_spesialisasi_values = [(id_user, spesialisasi_map[kategori]) for kategori in kategori]
-            cursor.executemany(query_inserti_pelatih_spesialisasi, pelatih_spesialisasi_values)
+            create_pelatih(cursor, id_user, kategori, tanggal_mulai)
             
             db_connection.commit()
+            # TODO: Reditect ke suatu page
             db_connection.close()
         except Exception as e:
             db_connection.rollback()
             messages.error(request, e)
             redirect('reg_log:register-coach')
     return render(request, 'coach.html')
+
+
 
 def portal(request):
     return render(request, 'portal.html')
@@ -105,3 +109,17 @@ def get_spesialisasi(cursor) -> dict[str:str]:
     result = cursor.fetchall()
     result = {nama:id for id, nama in result}
     return result
+
+def create_pelatih(cursor, id_user, kategori, tanggal_mulai):
+    query_insert_pelatih = """INSERT INTO pelatih (id, tanggal_mulai)
+            VALUES (%s, %s)"""
+    cursor.execute(query_insert_pelatih, (id_user, tanggal_mulai))
+            
+    create_spesialisasi_relasi(cursor, id_user, kategori)
+
+def create_spesialisasi_relasi(cursor, id_user, kategori):
+    spesialisasi_map = get_spesialisasi(cursor)
+    query_inserti_pelatih_spesialisasi = """INSERT INTO pelatih_spesialisasi (id_pelatih, id_spesialisasi)
+            VALUES (%s, %s)"""
+    pelatih_spesialisasi_values = [(id_user, spesialisasi_map[kategori]) for kategori in kategori]
+    cursor.executemany(query_inserti_pelatih_spesialisasi, pelatih_spesialisasi_values)
