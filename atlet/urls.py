afbb35f56bda34caf8935532ad6f267e9419f2dd
AndiRaihan <dzaky.raihan@gmail.com>
2023-05-17 20:38:49 +0700

    Finished pick stadium page

--- atlet/urls.py
+++ atlet/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from atlet.views import daftar_stadium
+from atlet.views import daftar_stadium, daftar_event, daftar_partai
 
 app_name = 'atlet'
 
 urlpatterns = [
     path('daftar-event/', daftar_stadium, name='daftar_stadium'),
+    path('daftar-event/<str:nama_stadium>/', daftar_event, name='daftar_event'),
+    path('daftar-event/<str:nama_stadium>/<str:nama_event>/<str:tahun_event>/', daftar_partai, name='daftar_partai'),
 ]
--- atlet/views.py
+++ atlet/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import psycopg2
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
 # Create your views here.
 
@@ -18,3 +20,33 @@ def daftar_stadium(request):
     context['stadium'] = cursor.fetchall()
     db_connection.close()
     return render(request, 'daftar_stadium.html', context=context)
+
+def daftar_event(request, nama_stadium):
+    db_connection = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="Inipasswordsaya12."
+    )
+    cursor = db_connection.cursor()
+    cursor.execute("set search_path to babadu")
+    event_list = get_event_in_stadium(nama_stadium, cursor)
+    context = {"daftar_event": event_list}
+    return render(request, 'daftar_event.html', context=context)
+
+def daftar_partai(request, nama_stadium, nama_event, tahun_event):
+    return render(request, 'daftar_partai.html')
+
+def get_event_in_stadium(nama_stadium, cursor):
+    query_get_event = """
+    SELECT E.nama_event, E.Total_hadiah, E.Tgl_mulai, E.Kategori_Superseries
+    FROM EVENT E
+    WHERE E.nama_stadium = %s
+    AND E.Tgl_Mulai > CURRENT_DATE
+    """
+    event_list = cursor.execute(query_get_event, (nama_stadium,))
+    event_list = cursor.fetchall()
+    event_list = [(event[0], locale.currency(event[1], grouping=True), 
+                   event[2].strftime('%d-%m-%Y'), event[3]) for event in event_list]
+                   
+    return event_list
